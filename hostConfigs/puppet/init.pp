ca4a6db75ccdb68068fa63b0fe01d6c575490192
Luis Cobelli <59371259+lcobelli@users.noreply.github.com>
2022-03-24 20:32:50 -0300

    configuracion compose

--- hostConfigs/puppet/init.pp
+++ hostConfigs/puppet/init.pp
@@ -6,10 +6,14 @@ class docker_install {
 # la aplicación así como también un archivo de configuración. Lo recomendando
 # es que lo que es propio de la aplicación sea un módulo distinto.
 
+exec { 'agrego-key':                    
+  command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'  
+} 
+
 
 # Agrego el repositorio para la instalación de Docker
 exec { 'agrego-repositorio':                    
-  command => '/usr/bin/add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'  
+  command => '/usr/bin/add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" '  
 } ->
 # Actualización de repositorio. La declaracion de un bloque exec permite definir
 # comandos que ejecutara el nodo cliente de Puppet
@@ -17,7 +21,8 @@ exec { 'apt-update':
   command => '/usr/bin/apt-get update'  
 }
 
-# Instalación del paquete docker. Tambien es para ejemplicar que se puede declarar
+# Instalación del paquete docker. Tambien
+ es para ejemplicar que se puede declarar
 # como requisito que se ejecuten una serie de comandos antes de la instalación
 package { 'docker-ce':
   require => Exec['apt-update','agrego-repositorio','apt-update'],       
@@ -25,13 +30,17 @@ package { 'docker-ce':
 }
 
 # Instalación del paquete docker-compose
-package { 'docker-compose':
-  require => Exec['apt-update'],        
-  ensure => installed,
+exec { 'compose-download':
+  command => '/usr/bin/curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
 }
+
+exec { 'compose-config':
+  require => Exec['compose-download'],
+  command => "/bin/chmod +x /usr/local/bin/docker-compose && ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"}
 
 # Aprovisionamiento de configuración para la aplicación. Con esta declaracion
 # se transfiere un archivo del servidor Puppet Master al nodo que contiene el agente
+/*
 file { "/var/www/utn-devops-app/myapp/.env":
 	mode => "0644",
     owner => 'root',
@@ -39,6 +48,7 @@ file { "/var/www/utn-devops-app/myapp/.env":
 	ensure => 'present', 
     source => 'puppet:///modules/docker_install/env',
 }
+*/
 
 # asegurar que el servicio docker se este ejecutando
 service { 'docker':
@@ -46,10 +56,9 @@ service { 'docker':
 }
 
 exec { 'config-app':
-  command => 'docker-compose -f /vagrant/docker/docker-compose.yml up -d',       
+  command => 'docker-compose -f /vagrant/docker/node/docker-compose.yml up -d',       
   path => '/usr/bin',  
-  onlyif => 'test $(docker ps |grep apache)',
+  onlyif => 'test $(docker ps |grep nodejs)',
 }
-
  
 }
